361da5ae948d7e104df119d260e641b3e5ba83d2
Mardoqueu Sousa <mdq767@gmail.com>
2023-02-14 12:37:29 -0300

    criando repositories

---
+++ src/main/java/com/sousa/mardoqueu/turmas/repository/MatriculaRepository.java
@@ -0,0 +1,8 @@
+package com.sousa.mardoqueu.turmas.repository;
+
+import com.sousa.mardoqueu.turmas.model.Matricula;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MatriculaRepository extends JpaRepository<Matricula, Long> {
+}
+
