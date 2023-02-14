601f87b2aaa29c57518cf61c277155af51bb12d0
Mardoqueu Sousa <mdq767@gmail.com>
2023-02-14 18:01:50 -0300

    teste para turma service para o metodo whenListarTurmasThenReturnSucess

---
+++ src/test/java/com/sousa/mardoqueu/turmas/service/MatriculaServiceTest.java
@@ -0,0 +1,16 @@
+package com.sousa.mardoqueu.turmas.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+class MatriculaServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void matricularAluno() {
+    }
+}
