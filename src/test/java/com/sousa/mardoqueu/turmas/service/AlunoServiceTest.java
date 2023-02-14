81fdf7aa8d2280d0d3d64a7f365b95ec407d77e4
Mardoqueu Sousa <mdq767@gmail.com>
2023-02-14 16:32:34 -0300

    teste para aluno service para o metodo whenListarAlunosThenReturnAListofAlunos

--- src/test/java/com/sousa/mardoqueu/turmas/service/AlunoServiceTest.java
+++ src/test/java/com/sousa/mardoqueu/turmas/service/AlunoServiceTest.java
@@ -8,6 +8,8 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
 
@@ -17,6 +19,9 @@ class AlunoServiceTest {
     public static final String NOME = "Mardoqueu Sousa Telvina";
     public static final String CPF = "123.456.789-00";
     public static final String EMAIL = "mardoqueu.sousa@example.com";
+
+    public static final int INDEX = 0;
+
     @InjectMocks
     private AlunoService service;
     @Mock
@@ -42,7 +47,19 @@ class AlunoServiceTest {
     }
 
     @Test
-    void listarAlunos() {
+    void whenListarAlunosThenReturnAListofAlunos() {
+        when(alunoRepository.findAll()).thenReturn(List.of(aluno));
+
+        List<Aluno> response = service.listarAlunos();
+
+        assertNotNull(response);
+        assertEquals(1, response.size());
+        assertEquals(Aluno.class, response.get(INDEX).getClass());
+
+        assertEquals(ID, response.get(INDEX).getId());
+        assertEquals(NOME, response.get(INDEX).getNome());
+        assertEquals(CPF, response.get(INDEX).getCpf());
+
     }
 
     private void startAluno(){
