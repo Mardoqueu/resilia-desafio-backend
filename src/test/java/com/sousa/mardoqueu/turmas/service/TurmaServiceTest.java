13ff63d7bedaa7a7ed427a3a2c232939fcd01d90
Mardoqueu Sousa <mdq767@gmail.com>
2023-02-14 17:24:26 -0300

    teste para turma service para o metodo whenListarTurmasThenReturnSucess

--- src/test/java/com/sousa/mardoqueu/turmas/service/TurmaServiceTest.java
+++ src/test/java/com/sousa/mardoqueu/turmas/service/TurmaServiceTest.java
@@ -9,6 +9,7 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
 import java.time.LocalDate;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
@@ -18,6 +19,9 @@ class TurmaServiceTest {
     public static final int ID = 1;
     public static final String NOME = "Turma de Java";
     public static final String CURSO = "Java";
+
+    public static final int INDEX = 0;
+
 
     LocalDate dataAtualEsperada = LocalDate.now();
 
@@ -49,7 +53,22 @@ class TurmaServiceTest {
     }
 
     @Test
-    void listarTurmas() {
+    void whenListarTurmasThenReturnSucess() {
+        when(turmaRepository.findAll()).thenReturn(List.of(turma));
+
+        List<Turma> response = turmaService.listarTurmas();
+
+        assertNotNull(response);
+        assertEquals(1, response.size());
+        assertEquals(Turma.class, response.get(INDEX).getClass());
+
+        assertEquals(ID, response.get(INDEX).getId());
+        assertEquals(NOME, response.get(INDEX).getNome());
+        assertEquals(CURSO, response.get(INDEX).getCurso());
+        assertEquals(dataAtualEsperada, response.get(INDEX).getDataInicio());
+        assertEquals(dataAtualEsperada, response.get(INDEX).getDataEncerramento());
+
+
     }
 
     private void startTurma(){
