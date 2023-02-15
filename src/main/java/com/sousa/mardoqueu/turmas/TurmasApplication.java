3a25fca35934e613f2c3426d7dfeddd2a4730187
Mardoqueu Sousa <mdq767@gmail.com>
2023-02-15 16:07:29 -0300

    adicionando repositories na inicializacao

--- src/main/java/com/sousa/mardoqueu/turmas/TurmasApplication.java
+++ src/main/java/com/sousa/mardoqueu/turmas/TurmasApplication.java
@@ -1,10 +1,19 @@
 package com.sousa.mardoqueu.turmas;
 
+import com.sousa.mardoqueu.turmas.repository.AlunoRepository;
+import com.sousa.mardoqueu.turmas.repository.MatriculaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TurmasApplication {
+
+	@Autowired
+	AlunoRepository alunoRepository;
+
+	@Autowired
+	MatriculaRepository matriculaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(TurmasApplication.class, args);
